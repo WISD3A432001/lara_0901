130fb962c67226d076c3a56919cd4319edb61d31
chenyirou <annie851216@gmail.com>
2017-11-22 11:02:55 +0800

    在 AdminPostsController的 destroy內刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -41,4 +41,10 @@ class AdminPostsController extends Controller
         return redirect()->route('admin.posts.index');
     }
 
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect()->route('admin.posts.index');
+    }
+
 }
